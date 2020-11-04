e2d0561940c4b524901bbc4891188e50e2ccaee0
jkhan01 <jkhan266@gmail.com>
2020-11-04 12:18:29 +0530

    Updated

--- bot1/actions/actions.py
+++ bot1/actions/actions.py
@@ -32,3 +32,24 @@ class ActionProvideGuidance(Action):
 
         return []
 
+class ActionProvideScope(Action):
+    def name(self) -> Text:
+        return "action_provide_scope"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        noStream = {'Music':"Here are some undergrad courses for music: \n Diploma in Music \n B.A. (Music) \n B.F.A (Music) \n B.P.A (Music) \n You can refer this link for more details: \n https://www.examsplanner.in/career-in-music/",
+                    'Drawing': "Here are some undergrad courses for drawing: \n Diploma in Drawing \n B.A. (Drawing) \n B.F.A. (Drawing) \n You can refere this link for more details: \n https://www.sarvgyan.com/courses/drawing-painting"}
+
+        stream = {'Science':"Here are some undergrad courses with Science: \n B.Sc. \n B.E. \n B.Tech \n M.B.B.S. \n B.Pharm \n B.H.M.S. \n For more details click on the link: \n https://www.sarvgyan.com/courses/science-courses",
+                    'Arts':"Here are some undergrad courses with Arts: \n B.A.\n L.L.B. \n B.M.M \n B.F.D. \n For more details click on the link below: \n https://www.upgrad.com/blog/career-options-after-12th-arts/",
+                    'Commerce': "Here are some undergrad courses with Commerce: \n B.Comm. \n B.M.S. \n B.B.A \n B.Economics \n For more details click on the link below: \n https://www.collegedekho.com/articles/career-options-after-12th-commerce-courses-and-eligibility/"}
+
+        if tracker.get_slot("nostream") == None:
+            dispatcher.utter_message(text=str(stream[tracker.get_slot("stream").capitalize()]))
+        else:
+            dispatcher.utter_message(text=str(noStream[tracker.get_slot("nostream").capitalize()]))
+
+        return []
