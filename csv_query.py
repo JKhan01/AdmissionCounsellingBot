35b1df5be289a2e9c90193f3b47f58cd9368b5e2
jkhan01 <jkhan266@gmail.com>
2020-11-04 11:11:13 +0530

    First Stable Bot

--- csv_query.py
+++ csv_query.py
@@ -7,9 +7,15 @@ data = pd.read_csv("College.csv")
 # replacing blank spaces with '_'  
 data.columns =[column.replace(" ", "_") for column in data.columns] 
 print(data.columns)
+pd.options.display.max_colwidth = 200
+# filtering with query method
+stream = "science" 
+percentage = 75.0
+location = ["Mumbai",'Pune']
+sakshi=data.query(f'Stream =="{stream.capitalize()}" and percentage >= {percentage} and Location == "{location[0]}" ') 
 
-# filtering with query method 
-sakshi=data.query('Stream =="Arts"') 
-  
+print (sakshi[0:1].college.values[0])
 # display 
-print(sakshi)
+# for i in range(1,5):
+#     print(sakshi[i].college)
+print (sakshi.shape)
